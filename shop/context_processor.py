2aab0fc4bf052ab13399b14ef324fe6c40fccecb
Westa GmbH <westagithub@gmail.com>
2025-09-01 23:34:32 +0200

    Update: * Add new context processor

--- shop/context_processor.py
+++ shop/context_processor.py
@@ -1,7 +1,11 @@
 from django.urls import reverse
 
-from shop.views import current_tour
+from shop.views import current_tour, get_user_role
 
 
 def get_current_tour(request):
     return {'current_tour': current_tour}
+
+
+def user_role(request):
+    return {'role':get_user_role(request.user.email)}
